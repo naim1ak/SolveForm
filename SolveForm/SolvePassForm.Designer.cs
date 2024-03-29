2b2f77f191162a07fc3aa3c6b9e969edf5a4930e
PRO\naim.ak <naim.ak@bdh.com.tr>
2024-03-29 11:25:30 +0300

    Add project files.

---
+++ SolveForm/SolvePassForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace SolveForm
+{
+    partial class SolvePassForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Solve = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Solve
+            // 
+            this.Solve.Location = new System.Drawing.Point(280, 17);
+            this.Solve.Name = "Solve";
+            this.Solve.Size = new System.Drawing.Size(94, 29);
+            this.Solve.TabIndex = 0;
+            this.Solve.Text = "Solve";
+            this.Solve.UseVisualStyleBackColor = true;
+            this.Solve.Click += new System.EventHandler(this.Solve_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(12, 12);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(243, 34);
+            this.txtPass.TabIndex = 1;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(280, 61);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(94, 29);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(91, 50);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(164, 27);
+            this.result.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "PassWord";
+            // 
+            // SolvePassForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 109);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.Solve);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "SolvePassForm";
+            this.Text = "Solve";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Solve;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SolveForm/SolvePassForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolveForm
+{
+    public partial class SolvePassForm : Form
+    {
+        public SolvePassForm()
+        {
+            this.MaximizeBox = false;
+            
+            
+            InitializeComponent();
+        }
+
+        private void Solve_Click(object sender, EventArgs e)
+        {
+            if (txtPass.Text.Trim() != "") {
+
+
+                var pass = SifreyiCozAES(txtPass.Text);
+                result.Text = pass;
+            }
+
+        }
+
+        private static byte[] _salt = Encoding.ASCII.GetBytes("o6806642kbM7c5");
+        private static string sharedSecret = "8F0930A7-ADEF-41CD-B311-572F72E02A49";
+        public static string SifreyiCozAES(string cipherText)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+                throw new ArgumentNullException("cipherText");
+            if (string.IsNullOrEmpty(sharedSecret))
+                throw new ArgumentNullException("sharedSecret");
+
+
+            RijndaelManaged aesAlg = null;
+
+
+            string plaintext = null;
+
+            try
+            {
+
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(sharedSecret, _salt);
+
+
+                byte[] bytes = Convert.FromBase64String(cipherText);
+                using (MemoryStream msDecrypt = new MemoryStream(bytes))
+                {
+
+                    aesAlg = new RijndaelManaged();
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                    aesAlg.IV = ReadByteArray(msDecrypt);
+
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+
+                            //uzmanim.net
+                            plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+            finally
+            {
+
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+            return plaintext;
+        }
+
+        private static byte[] ReadByteArray(Stream s)
+        {
+            byte[] rawLength = new byte[sizeof(int)];
+            if (s.Read(rawLength, 0, rawLength.Length) != rawLength.Length)
+            {
+                throw new SystemException("Stream did not contain properly formatted byte array");
+            }
+
+            byte[] buffer = new byte[BitConverter.ToInt32(rawLength, 0)];
+            if (s.Read(buffer, 0, buffer.Length) != buffer.Length)
+            {
+                throw new SystemException("Did not read byte array properly");
+            }
+
+            return buffer;
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            txtPass.Text = "";
+            result.Text = "";
+        }
+    }
+}
